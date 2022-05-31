40d5db563be8295e8595205fb1b1e63d0e0be646
vijender-singh <singh.vijender@gmail.com>
2022-05-31 17:32:34 -0400

    markdownUpdated

--- install_needed_packages.R
+++ install_needed_packages.R
@@ -1,35 +1,39 @@
-# Code in this directory will help to install required packages from
+# Code in this directory will help to install required packages from CRAN or Bioconductor repositories
 
-
-#' I think it is good idea to make the installation and loading process easy
-#' Was 'rlang' package installed on your computer?. I got an error for ewastools without rlang,
-#' so I included rlang package to the list
-#'
 
 #' For Cran packages
+#pkgs is a vector of package names that are required from CRAN
 install_cran_pkgs <- function(pkgs){
-  not_installed <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
-  if(length(not_installed)){
-    install.packages(not_installed, dependencies = TRUE) # you may get a warning for ewas here, just ignore that
-    if("ewastools" %in% pkgs & "ewastools" %in% not_installed){
+  # installed.packages()[,"Package"] will list all the packages that are installed
+  # The whole code will create vector not_installed with missing packages.
+  missing_pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
+  # if statement checks if missing_pkgs have any elements in it.
+  # if there are it proceeds inside the function for installation.
+  if(length(missing_pkgs)){
+    install.packages(missing_pkgs, dependencies = TRUE) 
+    # you may get a warning for ewas here, just ignore that
+    # as ewastools is available from github
+    if("ewastools" %in% pkgs & "ewastools" %in% missing_pkgs){
+      # Package source : https://github.com/hhhh5/ewastools
       devtools::install_github("hhhh5/ewastools@master")
     }
   }else{
+    # missing_pkgs vector is empty
     message("Cran packages you need are installed")
   }
 }
 
 #' For bioconductor packages
+# The code executes exactly as above except it uses install function of
+# BiocManager to install the packages.
 install_bioconductor_pkgs <- function(pkgs){
-  bioc_not_installed <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
-  if(length(bioc_not_installed)){
-    BiocManager::install(c(bioc_not_installed))
+  bioc_missing_pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
+  if(length(bioc_missing_pkgs)){
+    BiocManager::install(c(bioc_missing_pkgs))
   }else{
     message("Bioconductor packages you need are installed")
   }
 }
-
-
 
 
 #' Run test to check if all packages are installed
@@ -37,9 +41,12 @@ install_bioconductor_pkgs <- function(pkgs){
 check_installed <- function(pkgs){
   not_installed <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
   if(length(not_installed)){
-    message("Package(s) not installed: ", not_installed)
+    message("Package(s) missing from list of installed packages: ", not_installed)
+    message("Please install missing packages manually and sort any issues")
+    message("Hopefully there are not many .... GOOD LUCK !!!")
   }else{
-    message("All good ...")
+    message("All Required Packages are installed")
+    message("Awesome lets have fun !!!!!")
   }
 }
 
